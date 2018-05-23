6f7864e198c25bedfc7450139fa21c0c15192e80
Caique <caique@Gingadmins-Mac-mini.local>
2018-05-23 19:52:24 -0300

    implementando material-ui

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
+// Material-UI
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+
 import Signin from './auth/signin/signin.component'
 import AuthRoute from './auth/route/auth-route.component'
 import AuthRedirect from './auth/redirect/auth-redirect.component';
@@ -10,14 +16,27 @@ import Home from './home/home.component'
 class App extends Component {
   render() {
     return (
-      <Router>
-        <content>
-          <Route exact path="/" component={Signin} />
-          <Route exact path="/signin" component={Signin} />
-          <Route exact path="/signout" component={AuthRedirect} />
-          <AuthRoute path="/home" component={Home} />
-        </content>
-      </Router>
+      <div>
+
+      <AppBar position="static">
+      <Toolbar>
+       <Typography variant="title" color="inherit">
+         Camunda UI POC
+       </Typography>
+      </Toolbar>
+      </AppBar>
+
+        <Router>
+          <content>
+            <Route exact path="/" component={Signin} />
+            <Route exact path="/signin" component={Signin} />
+            <Route exact path="/signout" component={AuthRedirect} />
+            <AuthRoute path="/home" component={Home} />
+          </content>
+        </Router>
+
+
+      </div>
     );
   }
 }
--- reactjs/src/auth/signin/signin.component.js
+++ reactjs/src/auth/signin/signin.component.js
@@ -3,6 +3,21 @@ import React from 'react'
 
 import { Component } from 'react'
 import { withRouter } from 'react-router-dom'
+// Material-UI
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+// Icons
+import LockIcon from '@material-ui/icons/Lock';
+import SendIcon from '@material-ui/icons/Send';
+// Transitions
+import Grow from '@material-ui/core/Grow';
 
 import Errors from '../../error/errors.component'
 import { signin } from '../'
@@ -10,86 +25,86 @@ import { signin } from '../'
 import './signin.component.css'
 
 const data = {
-    username: '',
-    password: '',
-    remember: false,
-    errors: []
+  username: '',
+  password: '',
+  remember: false,
+  errors: []
 }
 
 class Signin extends Component {
-    constructor(props) {
-        super(props)
-        this.state = data
-        this.changeUsername = this.changeUsername.bind(this)
-        this.changePassword = this.changePassword.bind(this)
-        this.changeRemember = this.changeRemember.bind(this)
-        this.addError = this.addError.bind(this)
-        this.clearErrors = this.clearErrors.bind(this)
-        this.onSubimit = this.onSubimit.bind(this)
-    }
+  constructor(props) {
+    super(props)
+    this.state = data
+    this.changeUsername = this.changeUsername.bind(this)
+    this.changePassword = this.changePassword.bind(this)
+    this.changeRemember = this.changeRemember.bind(this)
+    this.addError = this.addError.bind(this)
+    this.clearErrors = this.clearErrors.bind(this)
+    this.onSubimit = this.onSubimit.bind(this)
+  }
 
-    changeUsername (e) {
-        this.setState({ username: e.target.value })
-    }
+  changeUsername (e) {
+    this.setState({ username: e.target.value })
+  }
 
-    changePassword (e) {
-        this.setState({ password: e.target.value })
-    }
+  changePassword (e) {
+    this.setState({ password: e.target.value })
+  }
 
-    changeRemember(e) {
-        this.setState({ remember: e.target.checked })
-    }
+  changeRemember(e) {
+    this.setState({ remember: e.target.checked })
+  }
 
-    clearErrors(e) {
-        this.setState({ errors: [] })
-    }
+  clearErrors(e) {
+    this.setState({ errors: [] })
+  }
 
-    addError(error) {
-        const errors = [error].concat(this.state.errors)
-        this.setState({ errors: errors })
-    }
+  addError(error) {
+    const errors = [error].concat(this.state.errors)
+    this.setState({ errors: errors })
+  }
 
-    onSubimit (e) {
-        e.preventDefault()
-        this.clearErrors()
-        signin(this.state)
-            .then(user => this.props.history.push('/home'))
-            .catch(err => this.addError(err))
-    }
+  onSubimit (e) {
+    e.preventDefault()
+    this.clearErrors()
+    signin(this.state)
+    .then(user => this.props.history.push('/home'))
+    .catch(err => this.addError(err))
+  }
 
-    render() {
-        return (
-            <div className="columns" id="login">
-                <div className="column is-7 left">
-                    <h1>
-                        camunda-node-external
-                    </h1>
-                    <h2>
-                        Sua melhor<br />experiência<br />em <strong> Gestão de<br />
-                        Sistemas
-                    </strong>
-                    </h2>
-                </div>
-                <div className="column is-5 right">
-                    <div className="caixaLogin">
+  render() {
+    return (
 
-                        <Errors errors={this.state.errors} onDelete={this.clearErrors} />
+    <Grid container justify={'center'} >
+      <Grid item xs={true} md={6} >
+      <div style={{ padding: 10 }}>
+        <form method="post" onSubmit={this.onSubimit}>
+          <Grow in={true}>
+          <Card>
+          <CardHeader title="Login" avatar={<Avatar > <LockIcon /> </Avatar>}/>
+            <CardContent>
+               <FormControl fullWidth>
+                  <TextField  id="username" label="Usuário"  value={this.state.username} onChange={this.changeUsername}  />
+                  <TextField  id="username" label="Senha"  value={this.state.password} onChange={this.changePassword} />
+                </FormControl>
+            </CardContent>
+            <CardActions>
+              <Grid container justify={'flex-end'} >
+                <Grid item>
+                <Button type="submit" variant="fab" color="secondary" aria-label="login"><SendIcon /></Button>
+                </Grid>
+              </Grid>
+            </CardActions>
+            <Errors errors={this.state.errors} onDelete={this.clearErrors} />
+          </Card>
+          </Grow>
+        </form>
+      </div>
+      </Grid>
+    </Grid>
 
-                        <form method="post" onSubmit={this.onSubimit}>
-                            <h1>Login</h1>
-                            <input type="text" placeholder="Insira seu Usuário" name="user" value={this.state.username} onChange={this.changeUsername} />
-                            <input type="password" placeholder="Insira sua Senha" name="passwd" value={this.state.password} onChange={this.changePassword} />
-                            <input id="lembrar" type="checkbox" onChange={this.changeRemember} /> Lembrar-me
-                            <button type="submit" className="button">ENTRAR</button>
-                        </form>
-                        <a href="/forgot" className="forget">Esqueceu a senha?</a>
-                        <h2>Não tem uma conta?</h2>
-                        <a href="/signup"> Criar conta </a>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+    )
+  }
 }
 
 export default withRouter(Signin)
